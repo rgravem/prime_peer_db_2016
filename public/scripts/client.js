feb7a21520b28833f3943dd40c05ae6682705fc7
matt <mattcllhn@gmail.com>
2016-09-23 11:46:51 -0500

    got info to DOM

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,19 @@
 console.log('js sourced');
+var globalArray=[];
+var myApp = angular.module('myApp',[]);
+myApp.controller('assignments',['$scope','$http',function($scope,$http){
+  console.log("NG working");
+  $http({
+    method:'GET',
+    url:'/assignments',
+  }).then(function(response){
+    console.log('this came back from server',response.data);
+  var array = response.data;
+  console.log('this is the array:',array);
+  $scope.dataToDom = array;
+  console.log('$scope.dataToDom',$scope.dataToDom);
+  });//then
 
-$(document).ready(function(){
-  console.log('doc rdy');
 
-  $('#outputDiv')
-});
+
+}]);//myApp controller
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var path = require('path');
 var mongoURI = 'mongodb://localhost:27017/assignments';
 mongoose.connect(mongoURI);
 
+
 var port = process.env.PORT || 3000;
 
 // var Mouse = require('../model/assignments.js');
@@ -16,12 +17,16 @@ app.use(bodyParser.json());
 app.listen(port, function(){
   console.log('server up on:', port);
 });
+app.get('/',function(req, res){
+  console.log(">>>>newest base url hit<<<<<<<");
+  res.sendFile(path.resolve('public/index.html'));
+});//app.get
 
 
 var userRouter = require('../routers/userRouter');
 
 // use routers
-app.use('/', userRouter);
+app.use('/assignments', userRouter);
 
 app.get('/test/:id?', function(req, res){
   console.log('in test route');
@@ -32,3 +37,4 @@ app.get('/test/:id?', function(req, res){
 
   res.send('ok');
 });
+app.use( express.static( 'public' ) );
